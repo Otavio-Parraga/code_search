8083e1d72726c696ed50a1d994ef27b9b3ba30a1
Otavio-Parraga <otavio-parraga@acad.pucrs.br>
2022-01-30 19:17:17 -0300

    Callbacks added

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from datasets import CodeSearchNetDataset
 from models import CodeSearchModel
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-data', '--data_dir', type=str, default='datasets/CodeSearchNet')
     parser.add_argument('-lan', '--language', type=str, default='javascript')
+    parser.add_argument('-out', '--output_dir', type=str, default='checkpoints')
 
     # main configs
     args = parser.parse_args()
@@ -26,9 +28,22 @@ if __name__ == '__main__':
     trainloader = DataLoader(train_dataset, batch_size=4, shuffle=True)
     valloader = DataLoader(val_dataset, batch_size=4, shuffle=False)
 
+    # callbacks
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=args.output_dir,
+        filename='model_{epoch}.ckpt',
+        save_top_k=3,
+        save_last=True,
+        verbose=True,
+        monitor='val_loss')
+
+    early_stop_callback = EarlyStopping('val_loss', patience=3)
+
     # creating trainer
-    trainer = pl.Trainer(gpus=3,
+    trainer = pl.Trainer(gpus=2,
                          max_epochs=10,
                          gradient_clip_val=0.5,
-                         strategy='ddp',)
+                         strategy='ddp',
+                         callbacks=[checkpoint_callback,
+                                    early_stop_callback])
     trainer.fit(model, trainloader, valloader)
