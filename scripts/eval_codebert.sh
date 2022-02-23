16965e0a994cf01e97665cf3d193fbafc3271420
Otavio-Parraga <otavio-parraga@acad.pucrs.br>
2022-02-23 14:53:26 -0300

    Adding final evaluation scripts

--- scripts/eval_codebert.sh
+++ scripts/eval_codebert.sh
@@ -18,4 +18,4 @@ lang='ruby'
 CUDA_VISIBLE_DEVICES=0 python evaluation.py -ckpt $ckpt_path/$lang/$model/model_epoch=0-v1.ckpt -lang $lang -ptm $ptm
 
 lang='python'
-CUDA_VISIBLE_DEVICES=0 python evaluation.py -ckpt $ckpt_path/$lang/$model/model_epoch=6.ckpt -lang $lang -ptm $ptm
+CUDA_VISIBLE_DEVICES=0 python evaluation.py -ckpt $ckpt_path/$lang/$model/best_model.ckpt -lang $lang -ptm $ptm
